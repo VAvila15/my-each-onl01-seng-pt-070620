4c73ed242df335ea6f368d0585d6cf37f4554e35
Victor Avila <avilavictor1570@gmail.com>
2020-07-17 19:34:28 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,9 +2,12 @@ def my_each(array)
   if block_given? 
      = 0
      
-  while i < array.length
-    yield(array[i])
-    i=i+1
+    while i < array.length
+      yield(array[i])
+      i=i+1
+    end
+    array
+  else
+    puts "4"
   end
-  array
 end
